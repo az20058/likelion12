079c77b56a6d5cccb11beff498c8385cb2a4ac95
YeChan Kim <az20058@naver.com>
2024-05-14 13:33:13 +0900

    5

---
+++ 실습5/session5/src/page.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+let SkyBox = styled.div`
+    background: rgb(0, 149, 212);
+    padding: 20px;
+    border-radius: 10px;
+`;
+
+function Page(){
+    return(
+        <>
+            <SkyBox/>
+        </>
+    )
+}
